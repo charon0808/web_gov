c6c27faa57e14c9c687de2130eb686ab4183bc53
charon0808 <8452823@163.com>
2020-01-11 15:15:53 +0800

    update

--- web_gov/login/views.py
+++ web_gov/login/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render,redirect
 
 def check_login(func):  # 自定义登录验证装饰器
     def warpper(request, *args, **kwargs):
-        is_login = request.session.get('is_login', False)
+        is_login = request.session.get('is_login', True)
         if is_login:
             return func(request, *args, **kwargs)
         else:
